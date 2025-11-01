e2ce88799b8500e4ae47d694af3567dfb13270d1
rana <ranaabdulmoeez987@gmail.com>
2025-11-01 16:22:04 +0500

    ...

--- backend/Fakebook/Fakebook/Database.h
+++ backend/Fakebook/Fakebook/Database.h
@@ -1,10 +1,8 @@
 #pragma once
-
 #include "User.h"
 #include "FriendNetwork.h"
+#include "Post.h"
 #include <vector>
-
-class Post;
 
 class Database {
 private:
--- backend/Fakebook/Fakebook/Post.h
+++ backend/Fakebook/Fakebook/Post.h
@@ -8,50 +8,53 @@ using namespace std;
 
 class Post {
 private:
-	string author;
-	time_t timestamp;
-	string text;
-	string url;
+    string author;
+    time_t timestamp;
+    string text;
+    string url;
+
 public:
-	Post(string _a, time_t _timestamp, string _t, string _u) {
-		author = _a;
-		timestamp = _timestamp;
-		text = _t;
-		url = _u;
-	}
-	string getText() {
-		return text;
-	}
-	string getUrl() {
-		return url;
-	}
-	time_t getTimeStamp() {
-		return timestamp;
-	}
-	void setText(const string& _t) {
-		text = _t;
-	}
-	void setUrl(const string& _u) {
-		url = _u;
-	}
-	void display() {
-		cout << "Author: " << author << ", Time: " << ctime(&timestamp) << endl;
-		cout << "Text: " << text << endl;
-		cout << "Photo / Video: " << url << endl;
-	}
-	void saveToFile(const string& filename) {
-		ofstream out(filename, ios::app);
-		out << author << "#" << text << "#" << timestamp << "#" << url << endl;
-		out.close();
-	}
-	void readFromFile(const string& filename) {
-		ifstream in(filename);
-		getline(in, author, '#');
-		getline(in, text, '#');
-		string time;
-		getline(in, time, '#');
-		timestamp = stol(time);
-		getline(in, url);
-		in.close();
-	}
+    Post(string _a, time_t _timestamp, string _t, string _u) {
+        author = _a;
+        timestamp = _timestamp;
+        text = _t;
+        url = _u;
+    }
+
+    string getText() { return text; }
+    string getUrl() { return url; }
+    time_t getTimeStamp() { return timestamp; }
+
+    void setText(const string& _t) { text = _t; }
+    void setUrl(const string& _u) { url = _u; }
+
+    void display() {
+        char buffer[26];
+        ctime_s(buffer, sizeof(buffer), &timestamp);  
+
+        cout << "Author: " << author << ", Time: " << buffer;
+        cout << "Text: " << text << endl;
+        cout << "Photo / Video: " << url << endl;
+    }
+
+    void saveToFile(const string& filename) {
+        ofstream out(filename, ios::app);
+        if (out.is_open()) {
+            out << author << "#" << text << "#" << timestamp << "#" << url << endl;
+        }
+        out.close();
+    }
+
+    void readFromFile(const string& filename) {
+        ifstream in(filename);
+        if (!in.is_open()) return;
+
+        getline(in, author, '#');
+        getline(in, text, '#');
+        string time;
+        getline(in, time, '#');
+        timestamp = stol(time);
+        getline(in, url);
+        in.close();
+    }
 };
