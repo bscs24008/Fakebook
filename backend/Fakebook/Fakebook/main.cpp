2b31d65cafaa92aae8f28ec4caed9de1aeda40e1
rana <ranaabdulmoeez987@gmail.com>
2025-11-01 16:23:40 +0500

    main

--- backend/Fakebook/Fakebook/main.cpp
+++ backend/Fakebook/Fakebook/main.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
 #include <string>
-#include "User.h"
-#include "utills.h"
 #include <vector>
+#include "FriendNetwork.h"
+#include "Database.h"
 
 using namespace std;
 
 int main()
 {
-	User* users;
-	int number_of_users;
-
-	ifstream input_file("Text.txt");
-
-	input_file >> number_of_users;
-
-	users = new User[number_of_users];
-	for (int i = 0; i < number_of_users; i++)
-	{
-		input_file >> users[i];
-	}
-
-	input_file.close();
-	
-	for (int i = 0; i < number_of_users; i++)
-	{
-		users[i].print();
-	}
-
-	save_users(users, number_of_users);
+	return 0;
 }
