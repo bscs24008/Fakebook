91edc95f1f9be1f6fb6160d05bcc52d5622e7757
MuhummadAhmedAamir <bscs24070@itu.edu.pk>
2025-11-01 15:14:35 +0500

    Added Post class with file save and reload

---
+++ backend/Fakebook/Fakebook/Post.h
@@ -0,0 +1,57 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+class Post {
+private:
+	string author;
+	time_t timestamp;
+	string text;
+	string url;
+public:
+	Post(string _a, time_t _timestamp, string _t, string _u) {
+		author = _a;
+		timestamp = _timestamp;
+		text = _t;
+		url = _u;
+	}
+	string getText() {
+		return text;
+	}
+	string getUrl() {
+		return url;
+	}
+	time_t getTimeStamp() {
+		return timestamp;
+	}
+	void setText(const string& _t) {
+		text = _t;
+	}
+	void setUrl(const string& _u) {
+		url = _u;
+	}
+	void display() {
+		cout << "Author: " << author << ", Time: " << ctime(&timestamp) << endl;
+		cout << "Text: " << text << endl;
+		cout << "Photo / Video: " << url << endl;
+	}
+	void saveToFile(const string& filename) {
+		ofstream out(filename);
+		out << author << "#" << text << "#" << timestamp << "#" << url << endl;
+		out.close();
+	}
+	void readFromFile(const string& filename) {
+		ifstream in(filename);
+		getline(in, author, '#');
+		getline(in, text, '#');
+		string time;
+		getline(in, time, '#');
+		timestamp = stol(time);
+		getline(in, url);
+		in.close();
+	}
+};
